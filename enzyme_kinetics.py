bed37f1a77b6b25a2107b2776a1a01d3e985f531
richard <yueyericardo@gmail.com>
2019-09-18 16:35:32 -0400

    clean code, add unit, add background information, add few markers

--- enzyme_kinetics.py
+++ enzyme_kinetics.py
@@ -11,12 +11,12 @@ from bokeh.plotting import figure
 slider_Ks = Slider(title="Ks", value=10, start=1, end=20, step=3)
 slider_kcat = Slider(title="kcat * 1e-3", value=100, start=60, end=200, step=20)
 slider_Ki = Slider(title="Ki", value=0, start=0, end=20, step=3)
-slider_K_ES_I = Slider(title="K_ES_I", value=0, start=0, end=100, step=10)
-slider_K_EI_S = Slider(title="K_EI_S", value=0, start=0, end=100, step=10)
+slider_K_ES_I = Slider(title="Kii", value=0, start=0, end=100, step=10)
+slider_K_EI_S = Slider(title="Kss", value=0, start=0, end=100, step=10)
 # Initial concentration
 slider_E0 = Slider(title="E0", value=200, start=0, end=300, step=50)
 slider_S0 = Slider(title="S0", value=500, start=400, end=600, step=50)
-slider_Inhi0 = Slider(title="I0", value=0, start=0, end=1500, step=100)
+slider_Inhi0 = Slider(title="I0", value=0, start=0, end=700, step=50)
 
 # Eq constant
 Ki = slider_Ki.value
@@ -47,6 +47,7 @@ E = E0
 # converge_tolerance
 tol = 1
 dt = 0.01
+ESI_scale = 50
 
 
 def get_newdata(kr, kf, kcat, kir, ki, kesir, kesi, keisr, keis, S, ES, EI, ESI, Inhi, E, P, Inhi0, E0, S0, dt):
@@ -80,7 +81,7 @@ def get_newdata(kr, kf, kcat, kir, ki, kesir, kesi, keisr, keis, S, ES, EI, ESI,
         y_S.append(S)
         y_ES.append(ES)
         y_EI.append(EI)
-        y_ESI.append(ESI)
+        y_ESI.append(ESI * ESI_scale)
         y_Inhi.append(Inhi)
         y_E.append(E)
         y_P.append(P)
@@ -112,76 +113,74 @@ p_P = plot_con_time.line('x', 'y', source=source_P, line_width=5, line_alpha=0.5
 p_S = plot_con_time.line('x', 'y', source=source_S, line_width=5, line_alpha=0.5, color='green', legend='S')
 p_Inhi = plot_con_time.line('x', 'y', source=source_Inhi, line_width=5, line_alpha=0.5, color='orange', legend='I')
 p_E = plot_con_time.line('x', 'y', source=source_E, line_width=3, line_alpha=1, color='deepskyblue', line_dash='dashed', legend='E')
-p_ES = plot_con_time.line('x', 'y', source=source_ES, line_width=1, line_alpha=1, color='darkgreen', line_dash='dashed', legend='ES')
+p_ES = plot_con_time.line('x', 'y', source=source_ES, line_width=1, line_alpha=1, color='green', line_dash='dashed', legend='ES')
 p_EI = plot_con_time.line('x', 'y', source=source_EI, line_width=1, line_alpha=1, color='tomato', line_dash='dashed', legend='EI')
-p_ESI = plot_con_time.line('x', 'y', source=source_ESI, line_width=1, line_alpha=1, color='silver', line_dash='dashed', legend='ESI (low)')
-plot_con_time.yaxis.axis_label = "Concentration"
-plot_con_time.xaxis.axis_label = "Time"
-
-Km = (kcat + kr)/kf
-Vm = kcat * E
-label_km = bokeh.models.Label(x=10, y=540,
-                              text='Km = {:.2f}'.format(Km), render_mode='css',
-                              border_line_color='black', border_line_alpha=1.0,
-                              background_fill_color='white', background_fill_alpha=1.0)
-plot_con_time.add_layout(label_km)
+p_ESI = plot_con_time.line('x', 'y', source=source_ESI, line_width=1, line_alpha=1, color='black', line_dash='dashed', legend='ESI * {}'.format(ESI_scale))
+plot_con_time.yaxis.axis_label = "Concentration  (mol / L)"
+plot_con_time.xaxis.axis_label = "Time  (s)"
 
 
-def get_v0(x_S0, kcat, kf, kr, E0, I0, Ki, K_ES_I, K_EI_S):
-    Km = (kcat + kr)/kf
-    Vm = kcat * E0
-
-    # competitive inhibition
-    if (Ki > 0 and K_ES_I == 0 and K_EI_S == 0):
-        Km = Km * (1 + I0 / Ki)
-
-    # non-competitive inhibition
-    if (Ki > 0 and K_ES_I > 0):
-        Vm = Vm / (1 + I0 / K_ES_I)
-
-    # un-competitive inhibition
-    if (Ki == 0 and K_ES_I > 0):
-        Km = Km / (1 + I0 / K_ES_I)
-        Vm = Vm / (1 + I0 / K_ES_I)
-
-    V0 = Vm * x_S0 / (Km + x_S0)
-    return V0
-
-
-def get_1_over_v0(x_1_over_S0, kcat, kf, kr, E0, I0, Ki, K_ES_I, K_EI_S):
+def get_1_over_v0(x_S0, x_1_over_S0, kcat, kf, kr, E0, I0, Ki, K_ES_I, K_EI_S):
     Km = (kcat + kr)/kf
     Vm = kcat * E0
     inhi_type = 'None Inhibition'
+    text_Km = 'Km = {:.2f}'.format(Km)
+    text_Vm = 'Vm = {:.2f}'.format(Vm)
 
     # competitive inhibition
     if (Ki > 0 and K_ES_I == 0):
         Km = Km * (1 + I0 / Ki)
         inhi_type = 'Competitive Inhibition'
+        text_Km = 'Km\' = {:.2f}'.format(Km)
 
     # non-competitive inhibition
     if (Ki > 0 and K_ES_I > 0):
         Vm = Vm / (1 + I0 / K_ES_I)
+        Km = Km * (1 + I0 / Ki) / (1 + I0 / K_ES_I)
+        text_Km = 'Km\' = {:.2f}'.format(Km)
+        text_Vm = 'Vm\' = {:.2f}'.format(Vm)
         inhi_type = 'Non-Competitive Inhibition'
 
     # un-competitive inhibition
     if (Ki == 0 and K_ES_I > 0):
         Km = Km / (1 + I0 / K_ES_I)
         Vm = Vm / (1 + I0 / K_ES_I)
+        text_Km = 'Km\' = {:.2f}'.format(Km)
+        text_Vm = 'Vm\' = {:.2f}'.format(Vm)
         inhi_type = 'Un-Competitive Inhibition'
 
-    x_S0 = 1/x_1_over_S0
+    x_over = 1 / x_1_over_S0
+    V_1_over_S0 = Vm * x_over / (Km + x_over)
     V0 = Vm * x_S0 / (Km + x_S0)
-    return (1 / V0), inhi_type
+
+    x_circle_x = -1 / Km
+    y_circle_y = 1 / Vm
+    xy_circle_half_Vm = (Km, Vm/2)
+
+    return V0, 1/V_1_over_S0, inhi_type, text_Km, text_Vm, x_circle_x, y_circle_y, xy_circle_half_Vm
 
 
 x_S0 = np.linspace(0.01, 2000, 10000)
-y_V0 = get_v0(x_S0, kcat, kf, kr, E0, Inhi0, Ki, K_ES_I, K_EI_S)
 x_1_over_S0 = np.linspace(-0.02, 0.02, 900)
-y_1_over_S0, inhi_type = get_1_over_v0(x_1_over_S0, kcat, kf, kr, E0, Inhi0, Ki, K_ES_I, K_EI_S)
+y_V0, y_1_over_S0, inhi_type, text_Km, text_Vm, x_circle_x, y_circle_y, xy_circle_half_Vm = get_1_over_v0(x_S0, x_1_over_S0, kcat, kf, kr, E0, Inhi0, Ki, K_ES_I, K_EI_S)
 source_V_S = ColumnDataSource(data=dict(x=x_S0, y=y_V0))
 source_1_over = ColumnDataSource(data=dict(x=x_1_over_S0, y=y_1_over_S0))
+source_circle_x = ColumnDataSource(data=dict(x=[x_circle_x], y=[0]))
+source_circle_y = ColumnDataSource(data=dict(x=[0], y=[y_circle_y]))
+source_circle__half_Vm = ColumnDataSource(data=dict(x=[xy_circle_half_Vm[0]], y=[xy_circle_half_Vm[1]]))
 
-label_inhi_type = bokeh.models.Label(x=10, y=480,
+label_km = bokeh.models.Label(x=10, y=540,
+                              text=text_Km, render_mode='css',
+                              border_line_color='black', border_line_alpha=1.0,
+                              background_fill_color='white', background_fill_alpha=1.0)
+label_vm = bokeh.models.Label(x=10, y=480,
+                              text=text_Vm, render_mode='css',
+                              border_line_color='black', border_line_alpha=1.0,
+                              background_fill_color='white', background_fill_alpha=1.0)
+plot_con_time.add_layout(label_km)
+plot_con_time.add_layout(label_vm)
+
+label_inhi_type = bokeh.models.Label(x=10, y=420,
                                      text=inhi_type, render_mode='css',
                                      border_line_color='black', border_line_alpha=1.0,
                                      background_fill_color='white', background_fill_alpha=1.0)
@@ -193,16 +192,21 @@ plot_V_S = figure(plot_height=200, plot_width=600,
                   toolbar_location=None,
                   y_range=[0, 45])
 p_V_S = plot_V_S.line('x', 'y', source=source_V_S, line_width=2, line_alpha=1, color='violet', legend='Initial Rate')
-plot_V_S.yaxis.axis_label = "V0"
-plot_V_S.xaxis.axis_label = "[S]"
+plot_V_S.circle('x', 'y', source=source_circle__half_Vm, fill_color="purple", line_color="black", size=8, legend='(Km, Vm/2)')
+
+plot_V_S.yaxis.axis_label = "V0  (mol / (s*L))"
+plot_V_S.xaxis.axis_label = "[S]  (mol / L)"
 
 plot_1_over = figure(plot_height=200, plot_width=600,
                      title="1/V0 vs 1/[S]",
                      tools="crosshair,reset,save,wheel_zoom",
                      toolbar_location=None,
-                     y_range=[-0.5, 0.5],
+                     y_range=[-1, 1],
                      x_range=[-0.02, 0.02])
 p_1_over = plot_1_over.line('x', 'y', source=source_1_over, line_width=2, line_alpha=1, color='blue', legend='1/V0')
+plot_1_over.circle('x', 'y', source=source_circle_x, fill_color="green", line_color="black", size=8, legend='- 1/Km')
+plot_1_over.circle('x', 'y', source=source_circle_y, fill_color='red', line_color="black", size=8, legend='1/Vm')
+
 plot_1_over.yaxis.axis_label = "1 / V0"
 plot_1_over.xaxis.axis_label = "1 / [S]"
 
@@ -245,32 +249,37 @@ def update_data(attrname, old, new):
     source_Inhi.data = dict(x=x_t, y=y_Inhi)
     source_E.data = dict(x=x_t, y=y_E)
     source_P.data = dict(x=x_t, y=y_P)
-    print(max(y_ES))
-    print(max(y_ESI))
-    Km = (kcat + kr)/kf
-    label_km.text = 'Km = {:.2f}'.format(Km)
 
-    y_V0 = get_v0(x_S0, kcat, kf, kr, E0, Inhi0, Ki, K_ES_I, K_EI_S)
-    y_1_over_S0, inhi_type = get_1_over_v0(x_1_over_S0, kcat, kf, kr, E0, Inhi0, Ki, K_ES_I, K_EI_S)
+    y_V0, y_1_over_S0, inhi_type, text_Km, text_Vm, x_circle_x, y_circle_y, xy_circle_half_Vm = get_1_over_v0(x_S0, x_1_over_S0, kcat, kf, kr, E0, Inhi0, Ki, K_ES_I, K_EI_S)
+    label_km.text = text_Km
+    label_vm.text = text_Vm
     source_V_S.data = dict(x=x_S0, y=y_V0)
     source_1_over.data = dict(x=x_1_over_S0, y=y_1_over_S0)
     label_inhi_type.text = inhi_type
+    source_circle_x.data = dict(x=[x_circle_x], y=[0])
+    source_circle_y.data = dict(x=[0], y=[y_circle_y])
+    source_circle__half_Vm.data = dict(x=[xy_circle_half_Vm[0]], y=[xy_circle_half_Vm[1]])
 
 
 for w in [slider_Ks, slider_kcat, slider_Ki, slider_K_ES_I, slider_K_EI_S, slider_E0, slider_Inhi0, slider_S0]:
     w.on_change('value', update_data)
 
-title = bokeh.models.Div(text="Enzyme Kinetics<br><br>")
+title_left = bokeh.models.Div(text="Without Inhibition<br><br>")
 title_inhi = bokeh.models.Div(text="Inhibition<br><br>")
-energy_png = bokeh.models.Div(text="<img src='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-06-CodeCogsEqn%20-1-.png'><br><br><br>")
-wave_png = bokeh.models.Div(text="<img src='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-06-CodeCogsEqn%20-3-.png'><br><br><br>")
-prob_png = bokeh.models.Div(text="<img src='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-06-CodeCogsEqn%20-2-.png'><br><br><br>")
-v_net_png = bokeh.models.Div(text="<img src='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-06-CodeCogsEqn%20-4-.png'><br><br><br>")
+title = bokeh.models.Div(text="<h1>Simulation of Enzyme Kinetics</h1><a target='_blank' href='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-18-Background_Information%20-2-.html'>Background Information</a><br><hr>",
+                         style={'font-size': '120%', 'width': '1450px', 'font-family': 'serif', 'color': 'black', 'text-align': 'center'})
+footer = bokeh.models.Div(text="<br><hr><br>Richard (Jinze) Xue <br>2019.09.18<br> <a target='_blank' href='https://github.com/yueyericardo/simuc'>Source code on Github</a><br>Department of Chemistry, Physical Chemistry Division, University of Florida",
+                          style={'font-size': '100%', 'width': '1450px', 'font-family': 'serif', 'color': 'black', 'text-align': 'center'})
+reaction_png = bokeh.models.Div(text="<br><br><br><br><br>Symbol Definition: <img src='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-18-143043.png' style='width: 310px; margin: auto; display: block'><br>")
+mm_equation_png = bokeh.models.Div(text="Michaelis-Menten Equation:<br><br> <img src='https://yyrcd-1256568788.cos.na-siliconvalley.myqcloud.com/yyrcd/2019-09-18-mm-equation.png' style='width: 150px; margin: auto; display: block'><br><br><br>")
 
 # Set up layouts and add to document
-left = column(children=[title, slider_Ks, slider_kcat, slider_E0, slider_S0, energy_png, wave_png, prob_png, v_net_png], sizing_mode='fixed', width=400)
+up_title = row(children=[title], sizing_mode='fixed', height=150, width=1600)
+down_footer = row(children=[footer], sizing_mode='fixed', height=130, width=1600)
+left = column(children=[title_left, slider_Ks, slider_kcat, slider_E0, slider_S0, reaction_png, mm_equation_png], sizing_mode='fixed', width=400)
 middle = column(children=[plot_con_time, plot_V_S, plot_1_over], sizing_mode='fixed', width=700)
-right = column(children=[title_inhi, slider_Ki, slider_K_ES_I, slider_K_EI_S, slider_Inhi0], sizing_mode='fixed', width=500)
-all_layout = row(left, middle, right)
+right = column(children=[title_inhi, slider_Ki, slider_K_ES_I, slider_K_EI_S, slider_Inhi0], sizing_mode='fixed', width=400)
+all_layout = column(up_title, row(left, middle, right), down_footer)
+
 curdoc().add_root(all_layout)
 curdoc().title = "Enzyme Kinetics"
