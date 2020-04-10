7161213a8c7c3caafc7d6c1ce3e4d79f30fec3e3
richard <yueyericardo@gmail.com>
2020-04-10 17:15:45 -0400

    add_id_for_header

--- apps/dash/assets/add_id_for_header.js
+++ apps/dash/assets/add_id_for_header.js
@@ -3,8 +3,10 @@ var headers = ["h1", "h2", "h3", "h4"]
 for (var i=0, h; h=headers[i]; i++) {  
   var tags = document.getElementsByTagName(h);
   for (var j=0, hi; hi=tags[j]; j++) {
-    hi.id = hi.innerHTML.toLowerCase().replace(/\s/g, "-");
+    // remove <> tab
+    var tmp = hi.innerHTML.toLowerCase().replace(/<.*?>/g, '')
+    // remove !@#$%$%$
+    tmp = tmp.replace(/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '')
+    hi.id = tmp.replace(/\s|\"/g, "-");
   }
 }
-
-
