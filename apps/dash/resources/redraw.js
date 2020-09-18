7f28ac52ebd7a2ba8d65d0c197fff77f06182cb3
richard <yueyericardo@gmail.com>
2020-09-18 01:47:47 -0400

    css and html

--- apps/dash/resources/redraw.js
+++ apps/dash/resources/redraw.js
@@ -1,3 +1,15 @@
+document.getElementById('toggle_code').addEventListener('click', code_toggle);
+var loader = document.getElementById('loader');
+
+function code_toggle(){
+  var pres = document.querySelectorAll('pre');
+  for (var i = 0; i < pres.length; i++) {
+    pres[i].style.display = pres[i].style.display == 'none' ? 'block' : 'none';
+  }
+}
+
+code_toggle()
+
 function redraw(){
   var figs = document.getElementsByClassName("js-plotly-plot")
   for (var i = 0; i < figs.length; i++) {
@@ -10,5 +22,6 @@ console.log("Start");
 setTimeout(function(){
     console.log("Redraw");
     redraw();
+    loader.style.display="none";
 }, 2000);
 console.log("End");
